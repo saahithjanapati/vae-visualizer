62364d32a2632883fc014f1cd23c2ee83225983a
Saahith Janapati <itzsaahith01@gmail.com>
2022-09-07 17:39:52 -0400

    changing graph and latent space sliders

--- final.py
+++ final.py
@@ -7,15 +7,21 @@ import plotly.express as px
 # TODO: make a gif maker class
 # TODO: make slider worrk one-way
 # TODO: integrate with model
-# TODO: plain latent space explorer (just adjust vector values and see what happens ???)
+# TODO: plain latent space explorer, plot on scatterplot with different color (just adjust vector values and see what happens ???)
 
 
-SLIDER_MIN = 0
-SLIDER_MAX = 9
-SLIDER_INITIAL_VALUE = 9
+EPOCH_SLIDER_MIN = 0
+EPOCH_SLIDER_MAX = 9
+EPOCH_SLIDER_INITIAL_VALUE = 3
+
+
 DATA_POINT_1 = None
 DATA_POINT_2 = None
-STATIC_GRAPH = px.scatter(x=[0, 1, 2, 3, 4], y=[0, 1, 4, 9, 16])
+
+
+GRAPH_DATA_X = [0, 1, 2, 3, 4]
+GRAPH_DATA_Y = [x**EPOCH_SLIDER_INITIAL_VALUE for x in GRAPH_DATA_X]
+GRAPH = px.scatter(x=GRAPH_DATA_X, y=GRAPH_DATA_Y)
 
 
 RADIO_BUTTONS = ["Data Point 1", "Data Point 2"]
@@ -23,26 +29,57 @@ INITIAL_RADIO_SELECTION = RADIO_BUTTONS[0]
 RADIO_SELECTION = RADIO_BUTTONS[0]
 
 
+
+####
+## Variables for latent space visualization
+LATENT_SPACE_DIM = 2
+LATENT_SPACE_MIN = 0
+LATENT_SPACE_MAX = 10
+LATENT_SPACE_SLIDER_STEP = 0.1
+LATENT_SPACE_SLIDER_INITIAL_VALUE = (LATENT_SPACE_MIN + LATENT_SPACE_MAX)/2
+LATENT_SPACE_MARKS = {i: str(i) for i in range(EPOCH_SLIDER_MIN, EPOCH_SLIDER_MAX+1)}
+
+
 app = Dash(__name__)
+
+latent_space_sliders = [
+        dcc.Slider(min=LATENT_SPACE_MIN,max=LATENT_SPACE_MAX, step=LATENT_SPACE_SLIDER_STEP, 
+        marks=LATENT_SPACE_MARKS, 
+        value=LATENT_SPACE_SLIDER_INITIAL_VALUE, id=f"latent-space-dim-{i}") for i in range(LATENT_SPACE_DIM)]
+
+
 app.layout = html.Div([
-    dcc.Graph(id="scatter-plot", figure=STATIC_GRAPH),
+    dcc.Graph(id="scatter-plot", figure=GRAPH),
     dcc.Slider(
-        min=SLIDER_MIN,
-        max=SLIDER_MAX,
+        min=EPOCH_SLIDER_MIN,
+        max=EPOCH_SLIDER_MAX,
         step=None,
-        marks={i: str(i) for i in range(SLIDER_MIN, SLIDER_MAX)},
-        value=SLIDER_INITIAL_VALUE, id="epoch-slider"),
-    html.H6(f"Epoch number: {SLIDER_INITIAL_VALUE}", id="epoch-label"),
+        marks={i: str(i) for i in range(EPOCH_SLIDER_MIN, EPOCH_SLIDER_MAX)},
+        value=EPOCH_SLIDER_INITIAL_VALUE, id="epoch-slider"),
+    html.H6(f"Epoch number: {EPOCH_SLIDER_INITIAL_VALUE}", id="epoch-label"),
     dcc.RadioItems(RADIO_BUTTONS, INITIAL_RADIO_SELECTION, id="radio_button"),
     html.H6(f"Data Point Being Edited: {INITIAL_RADIO_SELECTION}", id="radio_info"),
     
-    
     html.H6( f"{RADIO_BUTTONS[0]} Value: {DATA_POINT_1}", id="point1"),
-    html.H6(f"{RADIO_BUTTONS[1]} Value: {DATA_POINT_2}", id="point2")])
+    html.H6(f"{RADIO_BUTTONS[1]} Value: {DATA_POINT_2}", id="point2")] + latent_space_sliders)
 
 
-# this callback will eventually change the graph and the sliders, and the gifs
-# pretty much everything on our dashboard
+
+# moidfy scatterplot graph to display individual latent space vector
+# individual callback for each input :(
+
+# def update_latent_vector(dim=0):
+    
+
+# for latent_dim in range(LATENT_SPACE_DIM):
+#     @app.callback(
+#         Output()
+#     )
+
+
+
+
+# change point selection radio button (for latent-space interpolation gifs)
 @app.callback(
     Output("radio_info", "children"),
     Input("radio_button", "value"))
@@ -53,6 +90,8 @@ def update_radio_selection(option):
     return f"Data Point Being Edited: {RADIO_SELECTION}"
 
 
+
+# select different epoch to visualize with the slider
 @app.callback(
     Output("epoch-label", "children"),
     Input("epoch-slider", "value"))
@@ -60,6 +99,20 @@ def update_epoch_number(epoch_number):
     return f"Epoch number: {epoch_number}" 
 
 
+
+# update graph with new epoch number
+@app.callback(
+    Output("scatter-plot", "figure"),
+    Input("epoch-slider", "value"))
+def update_graph(epoch_number):
+    global GRAPH_DATA_X, GRAPH_DATA_Y, GRAPH
+    GRAPH_DATA_X = [0, 1, 2, 3, 4]
+    GRAPH_DATA_Y = [x**epoch_number for x in GRAPH_DATA_X]
+    GRAPH = px.scatter(x=GRAPH_DATA_X, y=GRAPH_DATA_Y)
+    return GRAPH
+
+
+# choose point on scatterplot for latent-space interpolation
 @app.callback(
     Output('point1', 'children'),
     Output('point2', 'children'),
@@ -76,6 +129,10 @@ def display_click_data(clickData):
     else:
         DATA_POINT_2 = (clickData["points"][0]["x"], clickData["points"][0]["y"])
     return str(DATA_POINT_1), str(DATA_POINT_2)
+
+
+
+
 
 # update graph according to epoch slider value
 # @app.callback(
